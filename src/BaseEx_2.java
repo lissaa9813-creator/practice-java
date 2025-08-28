ce722c67e5486d6f33a03da1989463d27c2367c2
hayoung <lissaa9813@gmail.com>
2025-08-28 17:48:24 +0900

    Add solution for 2025-08-28

---
+++ src/BaseEx_2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseEx_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        Map<Integer,String> map = new HashMap<>();
+        map.put(10,"A");
+        map.put(11,"B");
+        map.put(12,"C");
+        map.put(13,"D");
+        map.put(14,"E");
+        map.put(15,"F");
+        map.put(16,"G");
+        map.put(17,"H");
+        map.put(18,"I");
+        map.put(19,"J");
+        map.put(20,"K");
+        map.put(21,"L");
+        map.put(22,"M");
+        map.put(23,"N");
+        map.put(24,"O");
+        map.put(25,"P");
+        map.put(26,"Q");
+        map.put(27,"R");
+        map.put(28,"S");
+        map.put(29,"T");
+        map.put(30,"U");
+        map.put(31,"V");
+        map.put(32,"W");
+        map.put(33,"X");
+        map.put(34,"Y");
+        map.put(35,"Z");
+
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int mod = 0;
+
+        while(!(n == 0) ){
+            mod = n % b ;
+            n = n / b ;
+
+            if(mod < 10){
+                sb.append(mod);
+            } else {
+                sb.append(map.get(mod));
+            }
+        }
+        for (int i = sb.length()-1; i >= 0 ; i--) {
+            bw.write(sb.charAt(i));
+        } bw.flush();
+    }
+}
---
+++ src/MathV1Ex_1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+
+public class MathV1Ex_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < input; i++) {
+            int quarterDiv = 0 ;
+            int dimeDiv = 0 ;
+            int nickelDiv = 0 ;
+            int pennyDiv = 0 ;
+            int change = Integer.parseInt(br.readLine());
+            if(change >= 25){
+                quarterDiv = change / 25 ;
+                change = change % 25;
+            }if(change >= 10) {
+                dimeDiv = change / 10;
+                change = change % 10;
+            }if(change >= 5) {
+                nickelDiv = change / 5;
+                change = change % 5;
+            }if(change >= 1) {
+                pennyDiv = change;
+            }
+            bw.write(quarterDiv+" "+dimeDiv+" "+nickelDiv+" "+pennyDiv);
+            bw.newLine();
+        } bw.flush();
+    }
+}
